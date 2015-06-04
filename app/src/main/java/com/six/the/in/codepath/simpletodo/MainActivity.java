64801b55278452bac1be230249ffaf034b7d6826
sshariff01 <shoabe.shariff@gmail.com>
2015-06-04 01:13:35 -0400

    Create a list with two hardcoded items

--- app/src/main/java/com/six/the/in/codepath/simpletodo/MainActivity.java
+++ app/src/main/java/com/six/the/in/codepath/simpletodo/MainActivity.java
@@ -1,17 +1,35 @@
 package com.six.the.in.codepath.simpletodo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items
+        );
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First item");
+        items.add("Second item");
     }
 
     @Override
